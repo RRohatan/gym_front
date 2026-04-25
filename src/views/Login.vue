38755c739870fbf8acd10094df6e1fd36ac70f58
OsnaiderJulioLopez <ojuliolopez02@correo.unicordoba.edu.co>
2026-04-24 19:35:15 -0500

    feat: eliminar íconos de entrada en el formulario de inicio de sesión para simplificar el diseño

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,9 +43,6 @@
           <div class="auth-field">
             <label class="auth-label">Correo electrónico</label>
             <div class="auth-input-wrap">
-              <svg class="auth-input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-              </svg>
               <input v-model="email" type="email" class="auth-input" placeholder="tu@correo.com" required />
             </div>
           </div>
@@ -53,9 +50,6 @@
           <div class="auth-field">
             <label class="auth-label">Contraseña</label>
             <div class="auth-input-wrap">
-              <svg class="auth-input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-              </svg>
               <input v-model="password" :type="showPassword ? 'text' : 'password'" class="auth-input pr-11" placeholder="••••••••" required />
               <button type="button" class="auth-eye-btn" @click="showPassword = !showPassword" tabindex="-1">
                 <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
